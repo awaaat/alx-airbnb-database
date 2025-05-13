3b63700102db577b067f4075fd375eee35974f3e
Allan <awaliaulah65@gmail.com>
2025-05-13 15:49:33 +0300

    added slight DB changes

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,15 +3,17 @@
 SELECT * 
 FROM booking as bk
 INNER JOIN "user" as usr
-ON bk.user_id = usr.user_id
+ON bk.user_id = usr.user_id;
 
 -- Left join of property and reviews
 SELECT * 
 FROM  property as pr
 LEFT JOIN review as rv
 ON pr.property_id = rv.property_id
+ORDER BY rv.rating DESC;
+
 --Full outer join of users and booking
 SELECT * 
 FROM "user" as usr
 FULL OUTER JOIN booking as bk 
-ON usr.user_id = bk.user_id
+ON usr.user_id = bk.user_id;
